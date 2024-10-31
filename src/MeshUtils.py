6bb3172048496ec076326e17dfe4c498ee57bf99
Antareep Kumar Sarma <antareep.sarma@epfl.ch>
2024-10-31 14:26:36 +0100

    Added meshutils correction

--- src/MeshUtils.py
+++ src/MeshUtils.py
@@ -32,8 +32,7 @@ class MeshUtils:
         ic += mesh3.number_nodes
         # we now add them to the mesh6 object
         mesh6.nodes = np.vstack((mesh3.nodes, new_nodes))
-        mesh6.connectivity = np.hstack(
-            (mesh3.connectivity, np.hstack((ic[:ne], ic[ne:2 * ne], ic[2 * ne:]))))
+        mesh6.connectivity = np.hstack((mesh3.connectivity, np.vstack((ic[:ne], ic[ne:2 * ne], ic[2 * ne:])).T))
         mesh6.number_nodes = len(mesh6.nodes)
 
         return mesh6
